af196e2bb7e5d15ea4c596dbc0a3202694bfd7a6
Jones <Jones@JONES>
2023-01-31 13:58:33 -0300

    Add project files.

---
+++ Infra/Data/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VendasWeb.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+    }
+}
